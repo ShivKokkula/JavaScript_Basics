c5980039d54e6c2fd7f2eff9339bb1c0879f7184
ShivKokkula <shivbk987@gmail.com>
2021-07-15 00:49:39 +0530

    Added Merge,sort,search and replace methods

--- Objects.js
+++ Objects.js
@@ -23,3 +23,37 @@ for(const carProp in myCar){
 
 delete myCar.year;
 console.log(myCar);
+
+// Merge Object
+
+let myMobile = {
+    brand : "Samsung",
+    price : 15000
+}
+
+let myProperties = {
+    ...myCar,
+    ...myMobile
+}
+
+console.log(myProperties);
+
+//Split
+const originalString = "How are you?";
+const splitString = originalString.split(" ");
+
+console.log(splitString);
+
+//Sort
+let myArray = [109, 136, 156, 188, 19, 190, 2, 34, 55, 90];
+console.log(myArray.sort());
+console.log(myArray.sort((a, b) => a - b))
+
+//Search
+
+console.log(myArray.includes(136));
+console.log(myArray.includes(90,1));
+console.log(myArray.indexOf(90));
+console.log(myArray.find(element => element < 5));
+console.log(myArray.filter(element => element > 100));
+
